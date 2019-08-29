b263ae9b6427af02d92591e996483fc1b282dd91
sumitnautiyal-sunbird <Rishabh.Kalra@niit.com>
2019-08-29 12:14:07 +0530

    tslint now allows to have different selectors for components

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
