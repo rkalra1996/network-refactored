e77510cfe034794c73b988fe49795f5464728b55
sumitnautiyal-sunbird <Rishabh.Kalra@niit.com>
2019-09-01 20:05:21 +0530

    removed unneccessary modal

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { GraphExporterComponent } from './components/graph-exporter/graph-exporter.component';
 import { SimpleBsDeleteModalComponent } from './components/simple-bs-delete-modal/simple-bs-delete-modal.component';
-import { CreateEditNodeModalComponent } from './components/create-edit-node-modal/create-edit-node-modal.component';
 
 @NgModule({
-  declarations: [GraphExporterComponent, SimpleBsDeleteModalComponent, CreateEditNodeModalComponent],
+  declarations: [GraphExporterComponent, SimpleBsDeleteModalComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
